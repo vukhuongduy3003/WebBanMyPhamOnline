42d1d42e04ec42d295b0631cfb94af538900264b
Tran Duc Hieu <tranduchieu42002>
2024-01-28 02:02:28 +0700

    fix request sanpham

--- BackEnd/src/main/java/com/vn/backend/controller/SanPhamController.java
+++ BackEnd/src/main/java/com/vn/backend/controller/SanPhamController.java
@@ -1,5 +1,6 @@
 package com.vn.backend.controller;
 
+import com.vn.backend.dto.SanPhamCreateRequest;
 import com.vn.backend.dto.filter.SanPhamFilter;
 import com.vn.backend.entity.DanhMuc;
 import com.vn.backend.entity.SanPham;
@@ -42,11 +43,22 @@ public class SanPhamController {
         return new ResponseEntity<>(entities, HttpStatus.OK);
     }
 
-    @PostMapping("/danhMucs/{idDanhMuc}")
-    public ResponseEntity<?> createSanPham(@PathVariable(value = "idDanhMuc") Integer idDanhMuc, @RequestBody SanPham form) {
-        DanhMuc danhMuc = danhMucService.getDanhMucByID(idDanhMuc);
+    @PostMapping()
+    public ResponseEntity<?> createSanPham( @RequestBody SanPhamCreateRequest form) {
+        DanhMuc danhMuc = danhMucService.getDanhMucByID(form.getIdDanhMuc());
+        SanPham sanPham = new SanPham();
+        sanPham.setTenSanPham(form.getTenSanPham());
+        sanPham.setSoLuong(form.getSoLuong());
+        sanPham.setGiaSanPham(form.getGiaSanPham());
+        sanPham.setGiaSale(form.getGiaSale());
+        sanPham.setHinhAnh(form.getHinhAnh());
+        sanPham.setNgaySanXuat(form.getNgaySanXuat());
+        sanPham.setNgayHetHan(form.getNgayHetHan());
+        sanPham.setMoTa(form.getMoTa());
+        sanPham.setTinhTrang(form.getTinhTrang());
+        sanPham.setDanhMuc(danhMuc);
         form.setDanhMuc(danhMuc);
-        service.createSanPham(form);
+        service.createSanPham(sanPham);
         return new ResponseEntity<String>("Create successfully!", HttpStatus.OK);
     }
 
